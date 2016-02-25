0fe39375e31131705c18db857a151f152570cefa
seaphy <seaphy@seaphymac.local>
2016-02-25 15:04:11 +0800

    init

---
+++ LHHOCStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LHHOCStudy
+//
+//  Created by seaphy on 16/2/25.
+//  Copyright © 2016年 seaphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

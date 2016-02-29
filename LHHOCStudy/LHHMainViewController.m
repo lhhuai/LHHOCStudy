70a426e606fc84f3921a81520a730a2cba54df87
seaphy <seaphy@seaphymac.local>
2016-02-29 11:39:26 +0800

    main

---
+++ LHHOCStudy/LHHMainViewController.m
@@ -0,0 +1,64 @@
+//
+//  ViewController.m
+//  LHHOCStudy
+//
+//  Created by seaphy on 16/2/25.
+//  Copyright © 2016年 seaphy. All rights reserved.
+//
+
+#import "LHHMainViewController.h"
+#import "LHHDefines.h"
+
+@interface LHHMainViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) UITableView *tableView;
+
+@end
+
+@implementation LHHMainViewController
+
+- (void)loadView {
+    [super loadView];
+    
+//    self.navigationItem.title = @"Study OC";
+    
+//    self.title = @"Study OC";
+    
+    CGRect tableViewFrame = CGRectMake(0, 0, SCREEN_WIDTH, VIEW_HIDETABBAR_HEIGHT);
+    _tableView = [[UITableView alloc] initWithFrame:tableViewFrame style:UITableViewStylePlain];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    
+    [self.view addSubview:_tableView];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    if (IOS_VERSION >= 7.0) {
+        self.edgesForExtendedLayout = UIRectEdgeNone;
+    } else {
+        self.wantsFullScreenLayout = YES;
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"kLHHIdentifier"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"kLHHIdentifier"];
+        cell.backgroundColor = [UIColor grayColor];
+    }
+    return cell;
+}
+
+@end
